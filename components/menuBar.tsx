bb0450289682f45325b9c179f37734f2e0b24253
Gungjay07 <agungpradityaaa@gmail.com>
2025-12-02 00:15:36 +0700

    add fitur  menu bar + shadow effect

--- components/menuBar.tsx
+++ components/menuBar.tsx
@@ -1,20 +1,33 @@
-"use client"
+"use client";
 
-import { div } from "motion/react-client"
+import { div } from "motion/react-client";
 import { Inter } from "next/font/google";
+import { useState } from "react";
 
-export default function MenuBar(){
-    return (
-      <div className={`grid grid-cols-3 gap-5 text-black text-center`}>
-        <div className="border-none  rounded-sm border-black bg-[#FEFAE7] p-2">
-          Menu Paket
-        </div>
-        <div className=" border-2 border-none rounded-sm border-black bg-[#FEFAE7] p-2">
-          Lauk
-        </div>
-        <div className=" border-2 border-none rounded-sm border-black  bg-[#FEFAE7] p-2">
-          Minuman
-        </div>
-      </div>
-    );
+export default function MenuBar() {
+  const [activeCategory, setActiveCategory] = useState("paket");
+  const categories = [
+    { id: "paket", label: "Menu Paket" },
+    { id: "lauk", label: "Lauk" },
+    { id: "minuman", label: "Minuman" },
+  ];
+
+  return (
+    <div className={`grid grid-cols-3 gap-5 text-black text-center`}>
+      {categories.map((category) => (
+        <button
+        key={category.id}
+        onClick={() => setActiveCategory(category.id)}
+          className={`border-none  rounded-sm border-black bg-[#FEFAE7] p-2
+         ${
+           activeCategory === category.id
+             ? "bg-[#FEFAE7] text-black font-bold text-l shadow-2xl shadow-gray-500"
+             : "bg-[#FEFAE7] text-gray-500"
+         }`}
+        >
+          {category.label}
+        </button>
+      ))}
+    </div>
+  );
 }
